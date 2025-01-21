5da6efbc2dc0f6faab791a33b405262b26925722
Sravan <s.sravankumar@enterpi.com>
2025-01-21 10:39:29 +0530

    folder structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-// import Header from "@/components/Header";
+import Header from "@/components/Header";
 // import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { after } from "next/server";
@@ -8,33 +8,33 @@ import { after } from "next/server";
 // import { eq } from "drizzle-orm";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
-//   const session = await auth();
+  //   const session = await auth();
 
-//   if (!session) redirect("/sign-in");
+  //   if (!session) redirect("/sign-in");
 
-//   after(async () => {
-//     if (!session?.user?.id) return;
+  //   after(async () => {
+  //     if (!session?.user?.id) return;
 
-//     const user = await db
-//       .select()
-//       .from(users)
-//       .where(eq(users.id, session?.user?.id))
-//       .limit(1);
+  //     const user = await db
+  //       .select()
+  //       .from(users)
+  //       .where(eq(users.id, session?.user?.id))
+  //       .limit(1);
 
-//     if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
-//       return;
+  //     if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+  //       return;
 
-//     await db
-//       .update(users)
-//       .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
-//       .where(eq(users.id, session?.user?.id));
-//   });
+  //     await db
+  //       .update(users)
+  //       .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+  //       .where(eq(users.id, session?.user?.id));
+  //   });
 
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
         {/* <Header session={session} /> */}
-
+        <Header />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
